b82c0c74cd05b13e2d820d849c7b4b7fb6c3e859
Tontowi <tontowijauhari285@gmail.com>
2023-09-28 22:13:23 +0700

    Identitas

--- jurnal01.java
+++ jurnal01.java
@@ -1,8 +1,9 @@
 public class jurnal01{
     public static void main(String[] args) {
-        System.out.println(x:"Nama: Mochammad Tontowi Jauhari");
-        System.out.println(x:"NIM: 607062330086");
-        System.out.println(x:"Kelas: D3IF-47-01");
+        System.out.println("Nama : Mochammad Tontowi Jauhari");
+        System.out.println("NIM : 607062330086");
+        System.out.println("Kelas :D3IF-47-01");
+
     }
 
 }
