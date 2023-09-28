de19eca04b70be7aa75cf0c62544a81bc9650116
Tontowi <tontowijauhari285@gmail.com>
2023-09-28 21:34:21 +0700

    initial commit

---
+++ jurnal01.java
@@ -0,0 +1,8 @@
+public class jurnal01{
+    public static void main(String[] args) {
+        System.out.println(x:"Nama: Mochammad Tontowi Jauhari");
+        System.out.println(x:"NIM: 607062330086");
+        System.out.println(x:"Kelas: D3IF-47-01");
+    }
+
+}
